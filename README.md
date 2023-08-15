39f9bb4ad737647022087c03acc0d2ec86156c22
cristhian villca mamani <cristhianvillcamamani@gmail.com>
2023-08-15 12:34:32 -0400

    app upea carreras

---
+++ README.md
@@ -0,0 +1,60 @@
+
+# Sistema de Administración Web para Carreras - Universidad Pública de El Alto
+
+Bienvenido al Sistema de Administración Web para Carreras de la Universidad Pública de El Alto. Este proyecto tiene como objetivo simplificar y mejorar la gestión de las carreras ofrecidas por la universidad a través de una interfaz web moderna y eficiente.
+
+# Características:
+
+Tecnologías Avanzadas: 
+
+El sistema está construido utilizando tecnologías de vanguardia, incluyendo Nuxt.js, Pinia, Vue.js y Webpack, lo que garantiza un rendimiento óptimo y una experiencia de usuario fluida.
+
+Diseño Responsivo: 
+
+La interfaz de usuario está diseñada para ser completamente receptiva, lo que permite a los usuarios acceder y utilizar el sistema desde una variedad de dispositivos, como computadoras de escritorio, tabletas y teléfonos móviles.
+
+Navegación Intuitiva: 
+
+Gracias a la integración de la biblioteca Swiper, la navegación por las diferentes secciones del sistema es intuitiva y atractiva visualmente.
+
+Seguridad Mejorada: 
+
+La seguridad de los datos y la información de los estudiantes es una prioridad. Hemos implementado la biblioteca crypto.js para garantizar la seguridad y confidencialidad de los datos sensibles.
+
+Gestión Integral: 
+
+Este sistema permite a los administradores de las carreras gestionar eficazmente la información de los estudiantes, horarios, asignaturas, profesores y más, todo desde una única plataforma centralizada.
+
+Documentación Completa:
+
+Junto con el código fuente, proporcionamos una documentación detallada sobre la configuración, instalación y uso del sistema, lo que facilita su implementación y personalización.
+
+Desarrollador: 
+
+CRISTHIAN VILLCA MAMANI - CRISMASVM
+
+linkedin https://www.linkedin.com/in/cristhian-villca-mamani-06933b251/
+
+
+## Instalación
+
+instalacion de las dependencias del proyecto usando npm 
+
+```bash
+  npm install 
+  
+  or 
+
+  npm install --legacy-peer-deps
+```
+
+## Despliegue
+
+para hacer un despliegue del proyecto usar el siguiente comando
+
+```bash
+
+  npm run dev
+  
+```
+
---
+++ nuxt.config.js
@@ -0,0 +1,104 @@
+export default {
+
+  /* =================================
+    CONFIGURACION DE LA APLICACION WEB
+
+    -contiene informacion sobre el seo 
+    -contiene el modo de despliegue para el mismo ya sea "static" o "server"
+  ====================================*/
+
+  target: 'server',
+
+  head: {
+    title: "UPEA | Universidad Publica de El Alto. ",
+    htmlAttrs: {
+      lang: "es",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "keywords",
+        name: "keywords",
+        content: "...",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "informacion de las carreras de la universidad publíca de el alto",
+      },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [
+      //{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap",
+      },
+    ],
+
+    script: [
+      {
+        src: "/js/wow.min.js",
+      },
+      { src: "/js/isotope.pkgd.min.js" },
+      { src: "/js/pace.min.js" },
+      { src: "/js/splitting.min.js" },
+      { src: "/js/simpleParallax.min.js" },
+    ],
+  },
+
+  // configuracon  global para el modo dark
+  css: ['@/assets/css/dark.css',],
+
+  // plugins implementados en el desarrollo de esta aplicación
+  plugins: [
+    "~/plugins/particles.js",
+    "~/plugins/vueAwesomeSwiper.js",
+    "~/plugins/vueSlickCarousel.js",
+    {
+      src: "~/plugins/vueEllipseProgress.js",
+      mode: "client",
+    },
+    {
+      src: "~/plugins/vueTyper.js",
+      mode: "client",
+    },
+    { src: '~/plugins/vue-pdf-embed.js', mode: 'client' },
+  ],
+
+  // implementacion de componentes en las distintas plantillas
+  components: true,
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/composition-api/module',
+    '@pinia/nuxt',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    ["vue-scrollto/nuxt", { duration: 1000 }],
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+  ],
+
+  // parametros para axios para la configuracion de axios
+  axios: {
+    baseURL: "https://serviciopagina.upea.bo",
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extractCSS: true,
+    splitChunks: {
+      layouts: true,
+    },
+  },
+
+  // ubicacion para el almacenamiento de la dist
+  buildDir: "dist",
+};
